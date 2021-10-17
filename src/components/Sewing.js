82e0b1e09cf2009f20c5b4bd16bc9a68f1947208
Russell Driver <driver.george85@gmail.com>
2021-10-17 09:19:58 +0100

    added sewing component content and styling

--- src/components/Sewing.js
+++ src/components/Sewing.js
@@ -1,9 +1,37 @@
+import butterfly from '../assets/butterfly.png';
 
 const Sewing = () => {
 
     return (
         <div className="sewing">
-            <h1>sewing</h1>
+            <h1>Sewing</h1>
+            <div className="sewingBox">
+                <h3 className="sewingBox-subHeading">Alterations</h3>
+                <div className="sewingBox-text">
+                    <img className="sewingBox-text-img" src={butterfly} alt="butterly" />
+                    <p className="sewingBox-text-p">
+                        Find the hidden beauty in your old garments or bring your favourite outfit back to life again.  Why do we have to get rid of the old when there is a hidden promise hiding somewhere in the old to reveal the new and also the loyalty of life long joy.
+                    </p>
+                </div>
+            </div>
+            <div className="sewingBox">
+            <h3 className="sewingBox-subHeading">New</h3>
+                <div className="sewingBox-text">
+                    <img className="sewingBox-text-img" src={butterfly} alt="butterly" />
+                    <p className="sewingBox-text-p">
+                        Made to order items that are cost effective and good quality.
+                    </p>
+                </div>
+            </div>
+            <div className="sewingBox">
+            <h3 className="sewingBox-subHeading">Curtains</h3>
+                <div className="sewingBox-text">
+                <img className="sewingBox-text-img" src={butterfly} alt="butterly" />
+                    <p className="sewingBox-text-p">
+                        Dress your home with unique curtains of your choice.
+                    </p>
+                </div>
+            </div>
         </div>
     )
 };
