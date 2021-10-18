14f16f1ee2e358875aaef0e436f473bc1be0b205
Russell Driver <driver.george85@gmail.com>
2021-10-18 07:31:05 +0100

    added form and form logic

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,105 @@
+import { isPending } from 'q';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Contact = () => {
 
+    let history = useHistory();
+    let [name, setName] = useState('');
+    let [email, setEmail] = useState('');
+    let [requestType, setRequestType] = useState('');
+    let [requestBody, setRequestBody] = useState('');
+    let [pending, setIsPending] = useState(false);
+
+    let handleSubmit = function(e) {
+        e.preventDefault();
+        let newRequest = {
+            name,
+            email,
+            requestType,
+            requestBody
+        };
+        setIsPending(true)
+
+        fetch('abc', {
+            method:'POST',
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(newRequest)
+        })
+        .then(() => {
+            console.log('submitted')
+            setIsPending(false);
+            history.push('/');
+        })
+
+        console.log(newRequest);
+    }
+
     return(
         <div className="contact">
-            <h1>contact</h1>
+            <h1>Contact</h1>
+            <p>Get in touch with us:</p>
+            <form className="contactForm"
+                onSubmit={handleSubmit}
+            >
+                <div className="contactForm-group">
+                    <div>
+                        <label>Name</label>
+                        <input
+                        type="text" 
+                        required
+                        value={name}
+                        onChange={(e) => {
+                            setName(e.target.value);
+                        }}
+                        />
+                    </div>
+                    <div>
+                        <label>Email</label>
+                        <input 
+                        type="text"  
+                        required
+                        value={email}
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                        }}
+                        />
+                    </div>
+                </div>
+                <div>
+                    <label>How can we help?</label>
+                    <select
+                        value={requestType}
+                        onChange={(e) => {
+                            setRequestType(e.target.value);
+                        }}
+                    >
+                        <option value="Sewing">Sewing</option>
+                        <option value="Etching">Etching</option>
+                        <option value="Gifts">Gifts</option>
+                        <option value="Lingerie">Lingerie</option>
+                    </select>
+                    <textarea 
+                    cols="30" 
+                    rows="10"
+                    required
+                    value={requestBody}
+                    onChange={(e) => {
+                            setRequestBody(e.target.value);
+                        }}
+                    >
+                    
+                    </textarea>
+                </div>
+               { !pending &&  <button>Send Message</button> }
+               { pending && <button disabled>Submitting....</button> }
+            </form>
+            <p>{name}</p>
+            <p>{email}</p>
+            <p>{requestBody}</p>
+            <p>{requestType}</p>
         </div>
     )
 };
--- src/components/Gifts.js
+++ src/components/Gifts.js
@@ -1,9 +1,37 @@
+import butterfly from '../assets/butterfly.png';
 
 const Gifts = () => {
 
     return (
         <div className="gifts">
-            <h1>gifts</h1>
+        <h1>Gifts</h1>
+            <div className="giftsBox">
+                <h3 className="giftsBox-subHeading">Glass Engraving</h3>
+                <div className="giftsBox-text">
+                    <img className="giftsBox-text-img" src={butterfly} alt="butterly" />
+                    <p className="giftsBox-text-p">
+                        Find the hidden beauty in your old garments or bring your favourite outfit back to life again.  Why do we have to get rid of the old when there is a hidden promise hiding somewhere in the old to reveal the new and also the loyalty of life long joy.
+                    </p>
+                </div>
+            </div>
+            <div className="giftsBox">
+                <h3 className="giftsBox-subHeading">Glass Etching</h3>
+                <div className="giftsBox-text">
+                    <img className="giftsBox-text-img" src={butterfly} alt="butterly" />
+                    <p className="giftsBox-text-p">
+                        Find the hidden beauty in your old garments or bring your favourite outfit back to life again.  Why do we have to get rid of the old when there is a hidden promise hiding somewhere in the old to reveal the new and also the loyalty of life long joy.
+                    </p>
+                </div>
+            </div>
+            <div className="giftsBox">
+                <h3 className="giftsBox-subHeading">Wood Burning</h3>
+                <div className="giftsBox-text">
+                    <img className="giftsBox-text-img" src={butterfly} alt="butterly" />
+                    <p className="giftsBox-text-p">
+                        Find the hidden beauty in your old garments or bring your favourite outfit back to life again.  Why do we have to get rid of the old when there is a hidden promise hiding somewhere in the old to reveal the new and also the loyalty of life long joy.
+                    </p>
+                </div>
+            </div>
         </div>
     )
 };
