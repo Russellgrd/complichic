fc5d58a4a5b1c25ece247114c741fdeb22e040a0
Russell Driver <driver.george85@gmail.com>
2021-11-12 07:25:14 +0000

    added pricing info to lingerie page

--- src/components/Lingerie.js
+++ src/components/Lingerie.js
@@ -54,7 +54,7 @@ const Lingerie = () => {
                     </div>
                     <div className="lingerieGallery-box-textandpricing">
                         <h3>Daisy Bra</h3>
-                        <p>Wireless for comfort yet getting suppoert where needed.  Unforunately, the colours and fabric/lace will vary due to availability.  The lace undies are super comfy and seamless on the sides.  You can purchase a set or individual items of your choice</p>
+                        <p>Wireless for comfort yet getting support where needed.  Unfortunately, the colours and fabric/lace will vary due to availability.  The lace undies are super comfy and seamless on the sides.  You can purchase a set or individual items of your choice</p>
                         <br/ >
                         <h3>prices per set:</h3>
                         <p>X small - £25</p>
@@ -81,49 +81,37 @@ const Lingerie = () => {
                         <div className="lingerieCard">
                             <div className="lingeriePhoto photo5">
                                 <div className="lingeriePhoto-textbox">
-                                    <p>Daisy Bra</p>
-                                    <p>Lacy Undies</p>
+                                    <p>Maya Bra</p>
+                                    <p>Ivy Panty</p>
                                 </div>
                             </div>
                         </div>
                         <div className="lingerieCard">
                             <div className="lingeriePhoto photo6">
                                 <div className="lingeriePhoto-textbox">
-                                    <p>Daisy Bra</p>
-                                    <p>Lacy Undies</p>
+                                    <p>Maya Bra</p>
                                 </div>
                             </div>
                         </div>
                         <div className="lingerieCard">
                             <div className="lingeriePhoto photo7">
                                 <div className="lingeriePhoto-textbox">
-                                    <p>Daisy Bra</p>
-                                    <p>Lacy Undies</p>
+                                    <p>Ivy Panty</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="lingerieGallery-box-textandpricing">
-                        <h3>Daisy Bra</h3>
-                        <p>Wireless for comfort yet getting suppoert where needed.  Unforunately, the colours and fabric/lace will vary due to availability.  The lace undies are super comfy and seamless on the sides.  You can purchase a set or individual items of your choice</p>
+                        <h3>Maya Bra</h3>
+                        <p>With the underwire for support and style for comfort.  You have a choice if padded or non-padded, totally your personal preference.</p>
+                        <p>Only available in size 36A at the moment</p>
                         <br/ >
-                        <h3>prices per set:</h3>
-                        <p>X small - £25</p>
-                        <p>Small - £25</p>
-                        <p>Medium - £29</p>
-                        <p>Large - £33</p>
+                        <p>prices per set £40</p>
+                        <p>Bra only £32</p>
+                        <p>Ivy Panty: between £8 - £12 depending on lace and fabric used and availability.</p>
+                        
                         <br/>
-                        <h3>Daisy Bra</h3>
-                        <p>X small - £17</p>
-                        <p>Small - £17</p>
-                        <p>Medium - £19</p>
-                        <p>Large - £21</p>
-                        <br/>
-                        <h3>Lacy Undies</h3>
-                        <p>X Small - £8</p>
-                        <p>Small - £8</p>
-                        <p>Medium - £10</p>
-                        <p>Large - £12</p>
+                        
                     </div>
                 </div>
 
@@ -132,49 +120,42 @@ const Lingerie = () => {
                         <div className="lingerieCard">
                             <div className="lingeriePhoto photo8">
                                 <div className="lingeriePhoto-textbox">
-                                    <p>Daisy Bra</p>
-                                    <p>Lacy Undies</p>
+                                    <p>Black Beauty Bra</p>
+                                    <p>Franky Panty</p>
                                 </div>
                             </div>
                         </div>
                         <div className="lingerieCard">
                             <div className="lingeriePhoto photo9">
                                 <div className="lingeriePhoto-textbox">
-                                    <p>Daisy Bra</p>
-                                    <p>Lacy Undies</p>
+                                    <p>Black Beauty Bra</p>
+                                    <p>Franky Panty</p>
                                 </div>
                             </div>
                         </div>
                         <div className="lingerieCard">
                             <div className="lingeriePhoto photo10">
                                 <div className="lingeriePhoto-textbox">
-                                    <p>Daisy Bra</p>
-                                    <p>Lacy Undies</p>
+                                    <p>Black Beauty Bra</p>
+                                    <p>Franky Panty</p> 
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="lingerieGallery-box-textandpricing">
-                        <h3>Daisy Bra</h3>
-                        <p>Wireless for comfort yet getting suppoert where needed.  Unforunately, the colours and fabric/lace will vary due to availability.  The lace undies are super comfy and seamless on the sides.  You can purchase a set or individual items of your choice</p>
+                        <h3>Black Beauty Bra</h3>
+                        <p>Padded with underwire and power bar for extra support and comfort.</p>
+                        <p>It comes in sizes 28B to 40DD</p>
+                        <p>Prices per set: from 28B - £35 to 40DD - £58 per set.</p>
+                        <p>Black Beauty Bra only: from 28B - £27 to 40DD - £42 </p>
                         <br/ >
-                        <h3>prices per set:</h3>
-                        <p>X small - £25</p>
-                        <p>Small - £25</p>
-                        <p>Medium - £29</p>
-                        <p>Large - £33</p>
-                        <br/>
-                        <h3>Daisy Bra</h3>
-                        <p>X small - £17</p>
-                        <p>Small - £17</p>
-                        <p>Medium - £19</p>
-                        <p>Large - £21</p>
-                        <br/>
-                        <h3>Lacy Undies</h3>
-                        <p>X Small - £8</p>
+                        <h3>Franky Panties:</h3>
+                        <p>XX small - £8</p>
                         <p>Small - £8</p>
                         <p>Medium - £10</p>
                         <p>Large - £12</p>
+                        <p>X Large - £14</p>
+                        <p>XX Large - £16</p>
                     </div>
                 </div>
                
@@ -183,57 +164,53 @@ const Lingerie = () => {
                         <div className="lingerieCard">
                             <div className="lingeriePhoto photo11">
                                 <div className="lingeriePhoto-textbox">
-                                    <p>Daisy Bra</p>
-                                    <p>Lacy Undies</p>
+                                    <p>Madalynne Bra</p>
+                                    <p>Esme Panty</p>
                                 </div>
                             </div>
                         </div>
                         <div className="lingerieCard">
                             <div className="lingeriePhoto photo12">
                                 <div className="lingeriePhoto-textbox">
-                                    <p>Daisy Bra</p>
-                                    <p>Lacy Undies</p>
+                                    <p>Madalynne Bra</p>
+                                    <p>Esme Panty</p>
                                 </div>
                             </div>
                         </div>
                         <div className="lingerieCard">
                             <div className="lingeriePhoto photo13">
                                 <div className="lingeriePhoto-textbox">
-                                    <p>Daisy Bra</p>
-                                    <p>Lacy Undies</p>
+                                    <p>Esme Panty</p>
                                 </div>
                             </div>
                         </div>
                         <div className="lingerieCard">
                             <div className="lingeriePhoto photo14">
                                 <div className="lingeriePhoto-textbox">
-                                    <p>Daisy Bra</p>
-                                    <p>Lacy Undies</p>
+                                    <p>Esme Panty</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="lingerieGallery-box-textandpricing">
-                        <h3>Daisy Bra</h3>
-                        <p>Wireless for comfort yet getting suppoert where needed.  Unforunately, the colours and fabric/lace will vary due to availability.  The lace undies are super comfy and seamless on the sides.  You can purchase a set or individual items of your choice</p>
+                        <h3>Madalynne Bra</h3>
+                        <p>Non-Padded with the underwire for softness and comfort fitting</p>
+                        <p>Comes in sizes: 32A to 42dd - £17 to £38</p>
                         <br/ >
-                        <h3>prices per set:</h3>
-                        <p>X small - £25</p>
-                        <p>Small - £25</p>
-                        <p>Medium - £29</p>
-                        <p>Large - £33</p>
-                        <br/>
-                        <h3>Daisy Bra</h3>
-                        <p>X small - £17</p>
-                        <p>Small - £17</p>
-                        <p>Medium - £19</p>
-                        <p>Large - £21</p>
-                        <br/>
-                        <h3>Lacy Undies</h3>
-                        <p>X Small - £8</p>
+                        <p>prices per set: £25 to £52</p>
+                        <p>Classic Panties:</p>
+                        <p>X small - £8</p>
                         <p>Small - £8</p>
                         <p>Medium - £10</p>
                         <p>Large - £12</p>
+                        <p>X-Large - £14</p>
+                        <br/>
+                        <h3>Esme Panties:</h3>
+                        <p>X small - £9</p>
+                        <p>Small - £9</p>
+                        <p>Medium - £11</p>
+                        <p>Large - £13</p>
+                        <p>X-Large - £15</p>
                     </div>
                 </div>
                
