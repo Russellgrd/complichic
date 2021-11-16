3c99dfaa2708164e4bf1c39e3efb8191a9df782a
Russell Driver <driver.george85@gmail.com>
2021-11-16 07:27:56 +0000

    ammended nav page to include a forward slash in all Link to tags i.e /about-

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,9 +8,9 @@ const Nav = () => {
             <div className="navMainBox">
                 <img className="navimg" src={navimg} alt="butterfly on finger" />
                 <ul className="navButtonBox">
-                    <li className="navButtonBox-li"><Link className="navButtonBox-li-link" to="about">About</Link></li>
-                    <li className="navButtonBox-li"><Link className="navButtonBox-li-link" to="lingerie">Lingerie</Link></li>
-                    <li className="navButtonBox-li"><Link className="navButtonBox-li-link" to="contact">Contact us</Link></li>
+                    <li className="navButtonBox-li"><Link className="navButtonBox-li-link" to="/about">About</Link></li>
+                    <li className="navButtonBox-li"><Link className="navButtonBox-li-link" to="/lingerie">Lingerie</Link></li>
+                    <li className="navButtonBox-li"><Link className="navButtonBox-li-link" to="/contact">Contact us</Link></li>
                 </ul>
             </div>            
         </div>
