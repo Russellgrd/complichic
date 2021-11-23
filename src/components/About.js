3a9ac95598587553c847078347bfb90053033b76
Russell Driver <driver.george85@gmail.com>
2021-11-23 05:19:22 +0000

    addad text to about page - owner name and email

--- src/components/About.js
+++ src/components/About.js
@@ -10,6 +10,16 @@ const About = () => {
                 <img className="about-text-img" src={butterfly} alt="black and white butterfly" />
                 <p className="about-text-p"> <span>CompliChic</span> focusses on adding finess to your wardrobe, house & special bespoke gifts.  The inspiration for CompliChic started in 2016 with a heartfilled passion to bring beauty into your home without the cost.  It was a very slow start, but with love and passion, I kept going with desire to share it with the world out there.</p>
             </div>
+            <div className="about-text2">
+                <h3>Future</h3>
+                <p>We are also working on more pages with a wide selection of gifts and clothing.  Watch this space.</p>
+                
+                <br/>
+                <p>Thank you for your patience and understanding.</p>
+                <h3 className="aboutOwner">Theresa</h3>
+                <h4 className="aboutTitle">Owner</h4>
+                <p>theresa@complichic.co.uk</p>
+            </div>
         </div>
     )
 };
