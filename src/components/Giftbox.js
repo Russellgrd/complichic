b77582b9c93c7138ce3c1422a8b0a144cd289d4e
Russell Driver <driver.george85@gmail.com>
2022-02-07 08:29:34 +0000

    amended hedhog title on gifts page

--- src/components/Giftbox.js
+++ src/components/Giftbox.js
@@ -5,7 +5,7 @@ const Giftbox = ({coaster}) => {
     return(
         <div className="giftbox">
             <div className="giftbox-left">   
-                <h2 className="giftbox-heading">Hedgehog Coasters</h2>
+                <h2 className="giftbox-heading">Hand crafted hedgehog coasters</h2>
                 <p className="giftbox-description">
                     Pricing: Set of 4: £12 and individual coasters: £3.50 each. 
                 </p>
